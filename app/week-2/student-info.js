30ba5ca36feeb01bc181b08e6030e14e2741b90c
thuduongn <nguyenthuduong22@gmail.com>
2024-09-24 09:12:08 -0600

    update

---
+++ app/week-2/student-info.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Thu Duong Nguyen</h1>
+        <Link href="https://github.com/thuduongn/cprg306-assignments">https://github.com/thuduongn/cprg306-assignments</Link>
+      </main>
+    );
+  }
