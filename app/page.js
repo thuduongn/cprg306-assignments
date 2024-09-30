6e6a547223a1767b8b6f74b2b154c602d3e27d9f
thuduongn <nguyenthuduong22@gmail.com>
2024-09-29 18:40:22 -0600

    week-3

--- app/page.js
+++ app/page.js
@@ -5,7 +5,10 @@ export default function Home() {
     <div className="w-screen h-screen bg-black text-white">
       <div className="px-48 py-20">
         <h1 className="text-4xl font-bold pb-8">CPRG 306: Web Development 2 - Assignments</h1>
-        <Link href="/week-2" className="hover:text-green-500 hover:underline">Week 2 Assignments</Link>
+        <ul>
+          <li><Link href="/week-2" className="hover:text-green-500 hover:underline">Week 2 Assignments</Link></li>
+          <li><Link href="/week-3" className="hover:text-green-500 hover:underline">Week 3 Assignments</Link></li>
+        </ul>
       </div>
     </div>
   );
