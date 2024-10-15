0e60cf9cf7deb04c1a3022685050c954cfd7a970
thuduongn <nguyenthuduong22@gmail.com>
2024-10-14 22:33:23 -0600

    week-5

--- app/page.js
+++ app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
           <li><Link href="/week-2" className="hover:text-green-500 hover:underline">Week 2 Assignments</Link></li>
           <li><Link href="/week-3" className="hover:text-green-500 hover:underline">Week 3 Assignments</Link></li>
           <li><Link href="/week-4" className="hover:text-green-500 hover:underline">Week 4 Assignments</Link></li>
+          <li><Link href="/week-5" className="hover:text-green-500 hover:underline">Week 5 Assignments</Link></li>
         </ul>
       </div>
     </div>
